4f193aee7350e67a686399de66ff8351dfb61e77
forry <forry@jifeng.online>
2024-01-07 22:07:02 +0800

    add files

--- prepare_experiment.sh
+++ prepare_experiment.sh
@@ -12,3 +12,9 @@ avgNum=`expr $totalNum / $CLI_NUM + 1`
 echo $avgNum
 
 split -l $avgNum /data/coursework/data/uniprotkb_proteome_UP000005640_2023_10_05.fasta /data/coursework/data/split_ids/datafile_part_
+
+scp  -y /data/coursework/data/split_ids/datafile_part_aa  ec2-user@ec2-35-176-93-106.eu-west-2.compute.amazonaws.com:/home/ec2-user/coursework/data/rawdata.fasta
+scp -y /data/coursework/data/split_ids/datafile_part_ab  ec2-user@ec2-3-11-209-4.eu-west-2.compute.amazonaws.com:/home/ec2-user/coursework/data/rawdata.fasta
+scp -y /data/coursework/data/split_ids/datafile_part_ac  ec2-user@ec2-3-9-251-72.eu-west-2.compute.amazonaws.com:/home/ec2-user/coursework/data/rawdata.fasta
+scp -y /data/coursework/data/split_ids/datafile_part_ad  ec2-user@ec2-35-178-89-67.eu-west-2.compute.amazonaws.com:/home/ec2-user/coursework/data/rawdata.fasta
+scp -y /data/coursework/data/split_ids/datafile_part_ae  ec2-user@ec2-35-179-98-206.eu-west-2.compute.amazonaws.com:/home/ec2-user/coursework/data/rawdata.fasta
